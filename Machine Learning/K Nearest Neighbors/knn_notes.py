aa3ec9de2056fca14b88afa7435f2bd4c0e5c091
Casey Macaluso <macaluso.casey@gmail.com>
2023-08-17 13:49:36 -0500

    adding K Nearest Neighbors notes and project

--- Machine Learning/K Nearest Neighbors/knn_notes.py
+++ Machine Learning/K Nearest Neighbors/knn_notes.py
@@ -1,2 +1,87 @@
 # K Nearest Neighbors Notes
 
+# Library Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data Imports
+df = pd.read_csv("Classified Data.csv", index_col=0)
+df.head()
+
+# Need to ensure that all our data is on the same scale for KNN
+# That way, we can more accurately determine the target class.
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+# Fitting the scalar to all but the target variable
+scaler.fit(df.drop('TARGET CLASS', axis=1))
+scaled_features = scaler.transform(df.drop('TARGET CLASS', axis=1))
+# Making a dataframe of our scaled features
+df_features = pd.DataFrame(scaled_features, columns = df.columns[:-1])
+df_features.head()
+
+# Creating our training and testing data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(df_features, df['TARGET CLASS'], test_size=0.30, random_state=101)
+
+# Creating our KNN model, starting with neighbors at 1 (we'll alter this later)
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 1)
+knn.fit(x_train, y_train)
+pred = knn.predict(x_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
+#               precision    recall  f1-score   support
+
+#            0       0.91      0.95      0.93       159
+#            1       0.94      0.89      0.92       141
+
+#     accuracy                           0.92       300
+#    macro avg       0.92      0.92      0.92       300
+# weighted avg       0.92      0.92      0.92       300
+
+# [[151   8]
+#  [ 15 126]]
+
+# Stats look pretty good here, but let's see if we can make them even better
+
+
+# Elbow Method to find best k-value for the model
+# Goal: plot the error rate (when prediction and actual don't match) and see at what k-value we get the lowest error rate
+error_rate = []
+
+for i in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(x_train, y_train)
+    pred_i = knn.predict(x_test)
+    error_rate.append(np.mean(pred_i != y_test))
+    
+plt.figure(figsize = (10,6))
+plt.plot(range(1,40), error_rate, color='green', ls='dashed', marker='o', markersize=10)
+plt.title('Error Rate vs K Value')
+
+# k = 18 seems like a good spot to be at error wise. Error becomes a little volatile after that. Plus, error is already pretty low
+
+knn = KNeighborsClassifier(n_neighbors = 18)
+knn.fit(x_train, y_train)
+pred = knn.predict(x_test)
+from sklearn.metrics import classification_report, confusion_matrix
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
+
+#               precision    recall  f1-score   support
+
+#            0       0.94      0.97      0.96       159
+#            1       0.97      0.93      0.95       141
+
+#     accuracy                           0.95       300
+#    macro avg       0.95      0.95      0.95       300
+# weighted avg       0.95      0.95      0.95       300
+
+# [[155   4]
+#  [ 10 131]]
+
+# Managed to make some minor improvements to our model by choosing that higher k-value!
--- Machine Learning/K Nearest Neighbors/knn_project.py
+++ Machine Learning/K Nearest Neighbors/knn_project.py
@@ -1,2 +1,89 @@
 # K Nearest Neighbors Project
 
+# Library Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Data Imports
+df = pd.read_csv('KNN_Project_data.csv')
+df.head()
+
+# Some exploratory DA to get a feel for the data
+sns.pairplot(df, hue='TARGET CLASS')
+
+# Need to scale our data for this model to work
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+# Fitting data on all but the target class
+scaler.fit(df.drop('TARGET CLASS', axis=1))
+scaled_features = scaler.transform(df.drop('TARGET CLASS', axis=1))
+# Dataframe of our scaled features
+df_features = pd.DataFrame(scaled_features, columns = df.columns[:-1])
+df_features.head()
+
+# Making our training and testing data (30% size)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(df_features, df['TARGET CLASS'], test_size=0.30, random_state=101)
+
+# KNN model, starting with 1 neighbor and will adjust later
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 1)
+knn.fit(x_train, y_train)
+predictions = knn.predict(x_test)
+
+# Metrics to assess model performance
+from sklearn.metrics import confusion_matrix, classification_report
+print(classification_report(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+
+#               precision    recall  f1-score   support
+
+#            0       0.73      0.72      0.72       152
+#            1       0.71      0.72      0.72       148
+
+#     accuracy                           0.72       300
+#    macro avg       0.72      0.72      0.72       300
+# weighted avg       0.72      0.72      0.72       300
+
+# [[109  43]
+#  [ 41 107]]
+
+# Definitely some chances for improvement here, so lets experiment to see what k-value will work best for us
+
+# Goal: plot the error rate (when prediction != actual) and see which k-value gives the lowest error rate
+error_rate = []
+for i in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors = i)
+    knn.fit(x_train, y_train)
+    predictions_i = knn.predict(x_test)
+    error_rate.append(np.mean(predictions_i != y_test))
+    
+plt.figure(figsize=(10,6))
+plt.plot(range(1,40), error_rate, color='green', ls='dashed', marker='o', markersize=8)
+plt.title('Error Rate vs K Value')
+plt.xlabel('K Value')
+plt.ylabel('Error Rate')
+# Let's do k = 31 (anything past doesn't really give much of a difference)
+
+knn = KNeighborsClassifier(n_neighbors = 31)
+knn.fit(x_train, y_train)
+predictions = knn.predict(x_test)
+print(classification_report(y_test, predictions))
+print(confusion_matrix(y_test, predictions))
+
+#               precision    recall  f1-score   support
+
+#            0       0.87      0.81      0.84       152
+#            1       0.82      0.87      0.84       148
+
+#     accuracy                           0.84       300
+#    macro avg       0.84      0.84      0.84       300
+# weighted avg       0.84      0.84      0.84       300
+
+# [[123  29]
+#  [ 19 129]]
+
+# Nice improvements from our original model here. We were able to improve the accuracy by about 12%,
+# And precision, recall and f1-scores all saw solid improvements as well.
